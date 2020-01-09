06e11a548ed1151373c69fbde94a0c675330785e
delrosariod <59617888+delrosariod@users.noreply.github.com>
2020-01-09 09:06:43 -0700

    Review of entity creation/set up + navigational properties

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Addtional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    {
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Album Title is required.")]
+        [StringLength(160, ErrorMessage = "Maximum length for Album Title is 160 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Artist ID is required.")]
+        public int ArtistId { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Release Year must be a positive number.")]
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Maximum length for Release Label is 50 characters.")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+        }
+
+        public virtual Artist Artist { get; set; }
+        //public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Addtional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    //identify the sql entity (table) this class maps
+    [Table("Artists", Schema = "dbo")]
+    public class Artist
+    {
+        private string _Name;
+
+        [Key]
+        public int ArtistId { get; set; }
+
+        [StringLength(120,ErrorMessage ="Artist name is limited to 120 characters.")]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+
+        //virtual navigational properties. (Do not contain Data)
+        //these properties form a virtual relationship between the entity classes.
+        //based on the ERD relationship between entities.
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }
+}
