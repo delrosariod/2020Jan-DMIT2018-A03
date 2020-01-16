db2f30eead75f1248066cb164282d7b4d7ac78c3
delrosariod <59617888+delrosariod@users.noreply.github.com>
2020-01-16 09:26:04 -0700

    Implementation of BLL Crud Methods

--- ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using ChinookSystem.Data.Entities;
 using ChinookSystem.DAL;
 using System.ComponentModel;
+using DMIT2018Common.UserControls;
 #endregion
 
 namespace ChinookSystem.BLL
@@ -15,6 +16,9 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class AlbumController
     {
+        //this List<T> will hold a series of error message strings. It will be used by MessageUserControl via the BusinessRuleException
+        private List<string> _reasons = new List<string>();
+        
         #region Queries
         [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<Album> Album_List()
@@ -61,10 +65,93 @@ namespace ChinookSystem.BLL
         #endregion
 
         #region Add,Update,Delete
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public int Album_Add(Album item)
+        {
+            //add
+            using (var context = new ChinookContext())
+            {
+                //additional logic
+                if(CheckReleaseYear(item))
+                {
+                    item.ReleaseLabel = string.IsNullOrEmpty(item.ReleaseLabel) ? null : item.ReleaseLabel;
 
+                    context.Albums.Add(item); //staging
+                    context.SaveChanges(); //actual commit to database
+                    return item.AlbumId; //instance now has the identity PK value.
+                }
+                else
+                {
+                    throw new BusinessRuleException("Validation error", _reasons);
+                }
+            }
+        }
+        //update
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public int Album_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                //additional logic
+                if (CheckReleaseYear(item))
+                {
+                    item.ReleaseLabel = string.IsNullOrEmpty(item.ReleaseLabel) ? null : item.ReleaseLabel;
+
+                    context.Entry(item).State = System.Data.Entity.EntityState.Modified; //staging
+                    return context.SaveChanges(); //actual commit to database and return rowsaffected
+                    
+                }
+                else
+                {
+                    throw new BusinessRuleException("Validation error", _reasons);
+                }
+            }
+        }
+        //An overload function because ODS is on the Entity Level
+        [DataObjectMethod(DataObjectMethodType.Delete,false)]
+        public int Album_Delete(Album item)
+        {
+            return Album_Delete(item.AlbumId);
+        }
+
+        //delete is a physical delete
+        public int Album_Delete(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                if (existing == null)
+                {
+                    throw new Exception("Record already has been removed, no additional removal was executed.");
+                }
+                context.Albums.Remove(existing);
+                return context.SaveChanges();
+            }
+        }
         #endregion
 
         #region Support Methods
+        private bool CheckReleaseYear(Album item)
+        {
+            bool isValid = true;
+            int releaseyear;
+            if(string.IsNullOrEmpty(item.ReleaseYear.ToString()))
+            {
+                isValid = false;
+                _reasons.Add("Release year is required");
+            }
+            else if(!int.TryParse(item.ReleaseYear.ToString(), out releaseyear))
+            {
+                isValid = false;
+                _reasons.Add("Release year is not a valid year number.");
+            }
+            else if (releaseyear < 1950 || releaseyear > DateTime.Today.Year)
+            {
+                isValid = false;
+                _reasons.Add(string.Format("Release year {0} is invalid, year must be between 1950 and today.", releaseyear));
+            }
+            return isValid;
+        }
         #endregion
     }
 
