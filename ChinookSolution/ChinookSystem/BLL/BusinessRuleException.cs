c3247c940b1723c7a844670badbbe1cfdd26e407
delrosariod <59617888+delrosariod@users.noreply.github.com>
2020-01-16 08:46:14 -0700

    Implementation of MessageUserControl.

--- ChinookSolution/ChinookSystem/BLL/BusinessRuleException.cs
+++ ChinookSolution/ChinookSystem/BLL/BusinessRuleException.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChinookSystem.BLL
+#region Additional Namespaces
+using System.Web;
+#endregion
+
+namespace DMIT2018Common.UserControls
 {
-    class BusinessRuleException
+    /// <summary>
+    /// Summary description for BusinessRuleException
+    /// </summary>
+    [Serializable]
+    public class BusinessRuleException : Exception
     {
+        public List<string> RuleDetails { get; set; }
+        public BusinessRuleException(string message, List<string> reasons)
+            : base(message)
+        {
+            this.RuleDetails = reasons;
+        }
     }
+
 }
